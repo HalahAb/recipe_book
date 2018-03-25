31a4cb642d1ccb65aa756b879c0af7f0d4d1bb49
Halah Abuateeq <halah.abuateeq@gmail.com>
2018-03-26 00:01:56 +0800

    Added login link on nav bar if logged out, and logout if logged in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,7 @@ class SessionsController < ApplicationController
      user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
+      signed_in = true
       redirect_to '/'
     else
       redirect_to '/login'
@@ -15,6 +16,7 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
+    signed_in = false
     redirect_to '/login'
   end
 
